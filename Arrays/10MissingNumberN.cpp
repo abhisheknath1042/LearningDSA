30d393d5d9c911a52c60ff2720875bd66337861f
Abhishek Nath <abhisheknath1042@gmail.com>
2022-08-14 02:02:37 +0530

    Array Easy Question
    Missing number in array

---
+++ Arrays/10MissingNumberN.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution{
+  public:
+    int MissingNumber(vector<int>& array, int n) {
+        // Your code goes here
+        int sum=0;
+        int arrSum=0;
+        
+        sum = (n*(n+1))/2;
+        
+        for(int i:array)
+        {
+            arrSum+=i;
+        }
+        
+        return (sum-arrSum);
+        
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        vector<int> array(n - 1);
+        for (int i = 0; i < n - 1; ++i) cin >> array[i];
+        Solution obj;
+        cout << obj.MissingNumber(array, n) << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
