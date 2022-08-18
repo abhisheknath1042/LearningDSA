db8b7b316f0ca25ea09091ac0a58d5b19be4756a
Abhishek Nath <abhisheknath1042@gmail.com>
2022-08-18 23:20:12 +0530

    Sort and Swap alternate elements in Vector

---
+++ Arrays/28SortnSwap.cpp
@@ -0,0 +1,74 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+vector<int> formatArray(vector<int> a,int n);
+int main(){
+	int t;
+	cin>>t;
+	while(t--){
+		int n;
+		cin>>n;
+		vector<int> a(n);
+		for(int i=0;i<n;i++)
+			cin>>a[i];
+		vector<int>  b;
+		b=formatArray(a,n);
+		int flag=1;
+		if(b.size()==n){
+			for(int i=1;i<n;i+=2)
+				if(b[i]<b[i-1])
+					flag=0;
+			if(flag==0)
+				cout<<"0\n";
+			else{
+				sort(a.begin(),a.end());
+				sort(b.begin(),b.end());
+				for(int i=0;i<n;i++){
+					if(a[i]!=b[i])
+						flag=0;
+				}
+				cout<<flag<<endl;
+			}
+		}
+		else
+			cout<<"0\n";
+	}
+}
+// } Driver Code Ends
+
+
+/*Complete the function below*/
+vector<int> formatArray(vector<int> a,int n)
+{
+//add code here.	
+  vector<int> result;
+  int flag=0;
+  
+  sort(a.begin(),a.end(),greater<int>());
+
+  
+  for(int i=0; i<n-1; i+=2)
+  {
+      swap(a.at(i), a.at(i+1));
+  }
+  
+  for(int i=0; i<n-1; i+=2)
+  {
+      if(a.at(i)<a.at(i+1))
+      {
+          flag+=1;
+      }
+      else
+      {
+        flag=0;
+        break;
+      }
+  }
+  
+  if(flag!=0)
+      result.push_back(1);
+  else
+      result.push_back(0);
+
+  return result;
+}
