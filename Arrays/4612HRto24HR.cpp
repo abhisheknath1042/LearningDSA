a6c83b64d73cd89e5ced99390e738fa7a5e27ffc
Abhishek Nath <abhisheknath1042@gmail.com>
2022-08-25 22:36:21 +0530

    Update 4612HRto24HR.cpp

--- Arrays/4612HRto24HR.cpp
+++ Arrays/4612HRto24HR.cpp
@@ -5,14 +5,27 @@ using namespace std;
 //Function to convert time from 12HR to 24HR with string as return
 
 string timeConversion(string s){
-    string p=s; //assigning new string with the value of string that is passed
-    int i,j;
-    if(s[8]=='P'){                   // checking if it is AM or PM
-       int x =(s[0]-'0')*10 + (s[1]-'0') +12;
-       p[0]= x/10 +'0';
-       p[1]= x%10 +'0';
-    }
-    return p.substr(0, p.size()-2);
+    string ans=s;
+       if(ans[8]=='P') // check if given time is PM
+       {
+           int x = (ans[0]-'0')*10+(ans[1]-'0')+12;
+           //cout<<x;
+           if(x!=24)
+           {
+                ans[0]=x/10+'0';
+                ans[1]=x%10+'0';
+           }
+       }
+       if(ans[8]=='A') // check if given time is AM
+       {
+           int y=(ans[0]-'0')*10+ans[1]-'0';
+           if(y==12)
+           {
+               ans[0]='0';
+               ans[1]='0';
+           }
+       }
+    return ans.substr(0, ans.size()-2);
 }
 
 int main()
