c7213a08216bc12592e145be2198b23012d249c1
Abhishek Nath <abhisheknath1042@gmail.com>
2022-08-29 22:21:45 +0530

    Smallest Subarray with Sum Greater than X - GFG

---
+++ Arrays/50SmallestSubArray.cpp
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+  public:
+
+    int smallestSubWithSum(int arr[], int n, int x)
+    {
+        // Your code goes here 
+       int ans = INT_MAX;
+        
+       for(int i = 0; i<n; i++)
+       {
+           int sum = arr[i];
+           int j = i+1;
+           int count = 1;
+           
+           while(sum<=x && j<n)
+           {
+               sum +=arr[j];
+               j++;
+               count++;
+           }
+           
+           if(sum>x && count<ans)
+           {
+               ans = count;
+           }
+       }
+       return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n,x;
+		cin>>n>>x;
+		int a[n];
+		for(int i=0;i<n;i++)
+		cin>>a[i];
+		Solution obj;
+		cout<<obj.smallestSubWithSum(a,n,x)<<endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
